7729b5d6cb0bf90fbd7e2c333debfa73dcc5f4de
Max <spacemax@yandex.ru>
2021-05-14 09:37:50 +0300

    Рабочая версия файла

---
+++ main.py
@@ -0,0 +1,45 @@
+from pyb import UART, LED
+
+import sensor, image, time, utime
+
+ledBlue = LED(2)
+ledBlue.on()
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.LCD)
+sensor.skip_frames(100)
+
+clock = time.clock()
+ledBlue.off()
+
+# параметры юарта
+uart = UART(3)
+uart.init(9600, bits=8, parity=None, stop=1, timeout_char=1000) # init with given parameters
+
+# Словарь меток
+tag_families = 0
+tag_families |= image.TAG36H11 # comment out to disable this family (default family)
+
+led_count = 0
+uart_count = 0
+uart_flag = True
+# рабочий цикл
+while(True):
+
+    img = sensor.snapshot() # получение фотографии
+    # поиск маркеров на фото
+    for tag in img.find_apriltags(families=tag_families):
+        uart.write(str(tag.id())) # отправление по юарт id маркера
+
+    # Индикация работы скрипта
+    led_count = led_count + 1
+    if led_count >=4:
+        ledBlue.on()
+        led_count = 0
+    else:
+        ledBlue.off()
+
+    # Задержка
+    time.sleep(0.1)
+
